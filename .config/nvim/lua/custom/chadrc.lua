92f760f1a3276766117e1787655137fd1205a5ca
oliver ding <oliverdding@outlook.com>
2023-06-11 14:12:26 +0800

    update

--- .config/nvim/lua/custom/chadrc.lua
+++ .config/nvim/lua/custom/chadrc.lua
@@ -5,17 +5,13 @@ M.mappings = require "custom.mappings"
 
 M.ui = {
   lsp_semantic_tokens = true,
-  transparency = true,
   telescope = { style = "bordered" },
   statusline = {
     theme = "minimal",
     separator_style = "block",
   },
-  tabufline = {
-    lazyload = false,
-  },
-  theme_toggle = { "gruvbox", "gruvbox_light" },
-  theme = "gruvbox",
+  theme_toggle = { "everforest_dark" },
+  theme = "everforest_dark",
   nvdash = {
     load_on_startup = true,
   },
