d02879de4e0e952d43707acf2938a36fb64a2c7d
oliver ding <oliverdding@outlook.com>
2023-07-08 12:33:58 +0800

    update git; update neovim: add dap/neotest

--- .config/nvim/lua/custom/mappings.lua
+++ .config/nvim/lua/custom/mappings.lua
@@ -2,25 +2,169 @@ local M = {}
 
 M.general = {
   n = {
-    [";"] = { ":", "enter command mode", opts = { nowait = true } },
-    ["<leader>rr"] = {
-      function ()
-        require('rest-nvim').run()
+    ["<leader>B"] = { "<cmd> enew <CR>", "New buffer" },
+  },
+}
+
+M.dapui = {
+  n = {
+    ["<leader>du"] = {
+      function()
+        require("dapui").toggle()
       end,
-      "rest run http"
+      "Toggle dapui",
     },
-    ["<leader>rp"] = {
-      function ()
-        require('rest-nvim').run(true)
+    ["<leader>de"] = {
+      function()
+        require("dapui").eval()
       end,
-      "rest preview http"
+      "Eval",
     },
-    ["<leader>rl"] = {
-      function ()
-        require('rest-nvim').last()
+  },
+  v = {
+    ["<leader>de"] = {
+      function()
+        require("dapui").eval()
       end,
-      "rest run last http"
+      "Eval",
     },
+  },
+}
+
+M.dap = {
+  n = {
+    ["<leader>bb"] = {
+      function()
+        require("dap").toggle_breakpoint()
+      end,
+      "Toggle breakpoint",
+    },
+    ["<leader>bc"] = {
+      function()
+        require("dap").set_breakpoint(vim.fn.input "Breakpoint condition: ")
+      end,
+      "Breakpoint condition",
+    },
+    ["<leader>bC"] = {
+      function()
+        require("dap").clear_breakpoints()
+      end,
+      "Clear breakpoint",
+    },
+    ["<leader>dc"] = {
+      function()
+        require("dap").continue()
+      end,
+      "Continue",
+    },
+    ["<leader>dr"] = {
+      function()
+        require("dap").restart()
+      end,
+      "Restart",
+    },
+    ["<leader>dl"] = {
+      function()
+        require("dap").run_last()
+      end,
+      "Run last",
+    },
+    ["<leader>ds"] = {
+      function()
+        require("dap").terminate()
+      end,
+      "Stop",
+    },
+    ["<leader>dC"] = {
+      function()
+        require("dap").run_to_cursor()
+      end,
+      "Run to cursor",
+    },
+    ["<leader>dI"] = {
+      function()
+        require("dap").step_into()
+      end,
+      "Step into",
+    },
+    ["<leader>do"] = {
+      function()
+        require("dap").step_over()
+      end,
+      "Step over",
+    },
+    ["<leader>dO"] = {
+      function()
+        require("dap").step_out()
+      end,
+      "Step out",
+    },
+    ["<leader>dk"] = {
+      function()
+        require("dap").up()
+      end,
+      "Up",
+    },
+    ["<leader>dj"] = {
+      function()
+        require("dap").down()
+      end,
+      "Down",
+    },
+    ["<leader>dp"] = {
+      function()
+        require("dap").pause()
+      end,
+      "Pause thread",
+    },
+  },
+}
+
+M.neotest = {
+  plugin = true,
+  n = {
+    ["<leader>tr"] = {
+      function()
+        require("neotest").run.run()
+      end,
+      "Run nearest test",
+    },
+    ["<leader>tf"] = {
+      function()
+        require("neotest").run.run(vim.fn.expand "%")
+      end,
+      "Run current file",
+    },
+    ["<leader>td"] = {
+      function()
+        require("neotest").run.run(vim.fn.input "Directory: ")
+      end,
+      "Run test in directory",
+    },
+    ["<leader>ts"] = {
+      function()
+        require("neotest").run.stop()
+      end,
+      "Run stop",
+    },
+    ["<leader>ta"] = {
+      function()
+        require("neotest").run.attach()
+      end,
+      "Run attach",
+    },
+    ["<leader>dt"] = {
+      function()
+        require("neotest").run.run { strategy = "dap" }
+      end,
+      "Debug nearest test",
+    },
+  },
+}
+
+M.disabled = {
+  n = {
+    ["<leader>b"] = "",
   },
 }
 
--- .config/nvim/lua/custom/plugins.lua
+++ .config/nvim/lua/custom/plugins.lua
@@ -1,39 +1,93 @@
-local overrides = require("custom.configs.overrides")
+local overrides = require "custom.configs.overrides"
 
 local plugins = {
-    {
-        "nvim-tree/nvim-tree.lua",
-        opts = overrides.nvimtree,
-    },
-    {
-        "nvim-treesitter/nvim-treesitter",
-        opts = overrides.treesitter,
-    },
-    {
-        "williamboman/mason.nvim",
-        opts = overrides.mason,
-    },
-    {
-        "neovim/nvim-lspconfig",
-        event = "VeryLazy",
-        dependencies = {
-            {
-                "jose-elias-alvarez/null-ls.nvim",
-                config = function()
-                    require "custom.configs.null-ls"
-                end,
-            },
-        },
+  {
+    "nvim-tree/nvim-tree.lua",
+    opts = overrides.nvimtree,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = overrides.treesitter,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = overrides.mason,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    event = "VeryLazy",
+    dependencies = {
+      {
+        "jose-elias-alvarez/null-ls.nvim",
         config = function()
-            require "plugins.configs.lspconfig"
-            require "custom.configs.lspconfig"
+          require "custom.configs.null-ls"
         end,
+      },
     },
-    {
-        "rest-nvim/rest.nvim",
-        ft = "http",
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+  {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+      {
+        "rcarriga/nvim-dap-ui",
+        opts = {},
+        config = function(_, opts)
+          local dap, dapui = require "dap", require "dapui"
+          dapui.setup(opts)
+          dap.listeners.after.event_initialized["dapui_config"] = function()
+            dapui.open()
+          end
+          dap.listeners.before.event_terminated["dapui_config"] = function()
+            dapui.close()
+          end
+          dap.listeners.before.event_exited["dapui_config"] = function()
+            dapui.close()
+          end
+        end,
+      },
+      {
+        "theHamsta/nvim-dap-virtual-text",
         config = true,
-    }
+      },
+    },
+  },
+  {
+    "nvim-neotest/neotest",
+    init = function()
+      require("core.utils").load_mappings "neotest"
+    end,
+    dependencies = {
+      { "nvim-neotest/neotest-go" },
+      { "rouge8/neotest-rust" },
+    },
+    config = function()
+      local neotest_ns = vim.api.nvim_create_namespace "neotest"
+      vim.diagnostic.config({
+        virtual_text = {
+          format = function(diagnostic)
+            local message = diagnostic.message:gsub("\n", " "):gsub("\t", " "):gsub("%s+", " "):gsub("^%s+", "")
+            return message
+          end,
+        },
+      }, neotest_ns)
+      require("neotest").setup {
+        status = {
+          virtual_text = true,
+        },
+        adapters = {
+          require "neotest-go" {
+            args = { "-timeout=60s" },
+            extra_args = { "-race", "-cover" },
+          },
+          require "neotest-rust",
+        },
+      }
+    end,
+  },
 }
 
 return plugins
