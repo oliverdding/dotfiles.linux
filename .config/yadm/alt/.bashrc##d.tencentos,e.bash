ad1ee5b3658ad06fb01c3f5657eaaabf331c1a5a
oliver ding <oliverdding@outlook.com>
2023-10-05 13:18:36 +0800

    update sdkman

--- .config/yadm/alt/.bashrc##d.tencentos,e.bash
+++ .config/yadm/alt/.bashrc##d.tencentos,e.bash
@@ -5,3 +5,7 @@
 eval "$(zoxide init bash)"
 eval "$(direnv hook bash)"
 eval "$(starship init bash)"
+
+#THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
+export SDKMAN_DIR="/data/home/oliverdding/.local/share/sdkman"
+[[ -s "/data/home/oliverdding/.local/share/sdkman/bin/sdkman-init.sh" ]] && source "/data/home/oliverdding/.local/share/sdkman/bin/sdkman-init.sh"
