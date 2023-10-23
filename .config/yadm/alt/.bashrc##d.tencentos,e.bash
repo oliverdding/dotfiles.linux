605a591cf92dce04e72f8ff5a5016cdf5a701a73
oliver ding <oliverdding@tencent.com>
2023-10-23 11:12:44 +0800

    添加KUBECONFIG配置

--- .config/yadm/alt/.bashrc##d.tencentos,e.bash
+++ .config/yadm/alt/.bashrc##d.tencentos,e.bash
@@ -6,6 +6,8 @@ eval "$(zoxide init bash)"
 eval "$(direnv hook bash)"
 eval "$(starship init bash)"
 
+export KUBECONFIG=$(echo $(ls ~/.kube/config.d/* 2>/dev/null) | sed 's/ /:/g')
+
 #THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
 export SDKMAN_DIR="/data/home/oliverdding/.local/share/sdkman"
 [[ -s "/data/home/oliverdding/.local/share/sdkman/bin/sdkman-init.sh" ]] && source "/data/home/oliverdding/.local/share/sdkman/bin/sdkman-init.sh"
