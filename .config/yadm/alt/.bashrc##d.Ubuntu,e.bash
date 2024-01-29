2ef962fdb7a1369e04ec8cc0d59bbc266230f9ba
oliver ding <oliverdding@tencent.com>
2024-01-29 21:31:31 +0800

    update fzf

--- .config/yadm/alt/.bashrc##d.Ubuntu,e.bash
+++ .config/yadm/alt/.bashrc##d.Ubuntu,e.bash
@@ -3,7 +3,7 @@
 export GPG_TTY=$(tty)
 gpg-connect-agent updatestartuptty /bye >/dev/null
 
-[[ -f /usr/share/fzf/key-bindings.bash ]] && source /usr/share/fzf/key-bindings.bash || warning "[$0] cannot find key-bindings.bash of fzf"
+[[ -f /usr/share/doc/fzf/examples/key-bindings.bash ]] && source /usr/share/doc/fzf/examples/key-bindings.bash || echo "[$0] cannot find key-bindings.bash of fzf"
 
 eval "$(zoxide init bash)"
 eval "$(direnv hook bash)"
--- .config/yadm/alt/.bashrc##d.tencentos,e.bash
+++ .config/yadm/alt/.bashrc##d.tencentos,e.bash
@@ -1,6 +1,6 @@
 [ -z "$PS1" ] && return
 
-[[ -f $HOME/.config/fzf/key-bindings.bash ]] && source $HOME/.config/fzf/key-bindings.bash || warning "[$0] cannot find key-bindings.bash of fzf"
+[[ -f $HOME/.config/fzf/key-bindings.bash ]] && source $HOME/.config/fzf/key-bindings.bash || echo "[$0] cannot find key-bindings.bash of fzf"
 
 eval "$(zoxide init bash)"
 eval "$(direnv hook bash)"
